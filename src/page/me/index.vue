a30c85863db0eedf07bd4afad7324227f1be8dc9
wangdongrui <wangdongrui@aicuishou.com>
2019-03-28 09:53:22 +0800

    空格

--- src/page/me/index.vue
+++ src/page/me/index.vue
@@ -29,6 +29,7 @@
           <el-tab-pane label="用户管理">
             <ButtonList/>
           </el-tab-pane>
+          
           <el-tab-pane label="配置管理">配置管理</el-tab-pane>
           <el-tab-pane label="角色管理">角色管理</el-tab-pane>
           <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
